1a9be8bde0641c36624d6689a9e6633833dd1726
Xinhao Yuan <xinhaoyuan@gmail.com>
2018-11-07 14:39:40 -0500

    diffiso support

--- src/fd_sched_basicpos.erl
+++ src/fd_sched_basicpos.erl
@@ -5,29 +5,64 @@
 %% scheduler callbacks
 -export([init/1, enqueue_req/2, dequeue_req/1, hint/2, to_req_list/1]).
 
--record(rw_state, {reqs :: array:arary()
-                  ,rng :: rand:state()}).
+-record(rw_state,
+        { reqs :: array:arary()
+        , rng :: rand:state()
+        , dequeue_counter :: integer()
+        , guidance :: [term()]
+        , seed :: term()
+        }).
 
 init(Opts) ->
-    Seed =
+    {Seed, IsExternal} =
         case proplists:get_value(seed, Opts, undefined) of
             undefined ->
                 R = rand:export_seed_s(rand:seed_s(exrop)),
                 io:format(user, "seed = ~p~n", [R]),
-                R;
-            V -> V
+                {R, false};
+            V ->
+                {V, true}
         end,
     #rw_state{
        reqs = array:new(),
-       rng = rand:seed_s(Seed)
+       rng = rand:seed_s(Seed),
+       dequeue_counter = 0,
+       seed =
+           case IsExternal of
+               true ->
+                   undefined;
+               false ->
+                   Seed
+           end,
+       guidance =
+           case IsExternal of
+               true ->
+                   proplists:get_value(guidance, Opts, []);
+               false ->
+                   []
+           end
       }.
 
-enqueue_req(#fd_delay_req{data = Data} = ReqInfo, #rw_state{reqs = Reqs, rng = Rng} =  State) ->
+new_priority(RI, Rng) ->
     {UP, NewRng} = rand:uniform_s(Rng),
-    P = math:pow(UP, maps:get(weight, Data, 1)),
+    {math:pow(UP, maps:get(weight, RI, 1)), NewRng}.
+
+enqueue_req(#fd_delay_req{data = Data} = ReqInfo, #rw_state{reqs = Reqs, rng = Rng} =  State) ->
+    {P, NewRng} = new_priority(Data, Rng),
     {ok, State#rw_state{reqs = array:set(array:size(Reqs), {ReqInfo, P}, Reqs), rng = NewRng}}.
 
-dequeue_req(#rw_state{reqs = Reqs} = State) ->
+dequeue_req(#rw_state{dequeue_counter = Cnt, guidance = [Cnt]} = State) ->
+    Seed = rand:export_seed_s(rand:seed_s(exrop)),
+    dequeue_req(State#rw_state{guidance = [{Cnt, Seed}], seed = Seed});
+dequeue_req(#rw_state{reqs = Reqs, dequeue_counter = Cnt, guidance = [{Cnt, SeedTerm} | G]} = State) ->
+    {NewReqs, NewRng} =
+        array:foldl(
+          fun (Index, {#fd_delay_req{data = Data} = RI, _}, {CurReqs, CurRng}) ->
+                  {NewP, NewRng} = new_priority(Data, CurRng),
+                  {array:set(Index, {RI, NewP}, CurReqs), NewRng}
+          end, {Reqs, rand:seed_s(SeedTerm)}, Reqs),
+    dequeue_req(State#rw_state{reqs = NewReqs, rng = NewRng, dequeue_counter = 0, guidance = G});
+dequeue_req(#rw_state{reqs = Reqs, dequeue_counter = Cnt} = State) ->
     {I, _} = array:foldl(
                fun (I, {_, P}, none) ->
                        {I, P};
@@ -42,8 +77,16 @@ dequeue_req(#rw_state{reqs = Reqs} = State) ->
     S = array:size(Reqs),
     {Req, _} = array:get(I, Reqs),
     NewReqs = array:resize(S - 1, array:set(I, array:get(S - 1, Reqs), Reqs)),
-    {ok, Req, State#rw_state{reqs = NewReqs}}.
+    {ok, Req, State#rw_state{reqs = NewReqs, dequeue_counter = Cnt + 1}}.
 
+hint({get_seed_info, Ref, From}, #rw_state{dequeue_counter = Cnt, seed = Seed} = State) ->
+    Reply = {Seed, Cnt},
+    io:format(user, "[FD] hint get_seed_info -> ~w~n", [Reply]),
+    From ! {Ref, Reply},
+    State;
+hint({set_guidance, Guidance}, State) ->
+    io:format(user, "[FD] hint set_guidance ~w~n", [Guidance]),
+    State#rw_state{dequeue_counter = 0, guidance = Guidance};
 hint(_, State) ->
     State.
 
