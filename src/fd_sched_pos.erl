0b089e77610c78c82b186f68fe659b5636e524da
Xinhao Yuan <xinhaoyuan@gmail.com>
2019-03-14 09:16:58 -0400

    tweaks

--- src/fd_sched_pos.erl
+++ src/fd_sched_pos.erl
@@ -10,7 +10,8 @@
         , rng                  :: rand:state()
         , dequeue_counter      :: integer()
         , guidance             :: [term()]
-        , variant              :: undefined | lazy | simrw
+        , variant              :: undefined | basic | lazy | simrw
+        , is_racing_fun        :: fun()
         , seed                 :: term()
         , priority_reset_count :: integer()
         , max_age              :: integer()
@@ -26,28 +27,28 @@ init(Opts) ->
             V ->
                 {V, true}
         end,
-    #state
-        { reqs = array:new()
-        , rng = rand:seed_s(Seed)
-        , dequeue_counter = 0
-        , seed =
-              case IsExternal of
-                  true ->
-                      undefined;
-                  false ->
-                      Seed
-              end
-        , guidance =
-              case IsExternal of
-                  true ->
-                      proplists:get_value(guidance, Opts, []);
-                  false ->
-                      []
-              end
-        , variant = proplists:get_value(variant, Opts, undefined)
-        , priority_reset_count = 0
-        , max_age = 1000 %% XXX make it configurable
-      }.
+    #state{ reqs = array:new()
+          , rng = rand:seed_s(Seed)
+          , dequeue_counter = 0
+          , seed =
+                case IsExternal of
+                    true ->
+                        undefined;
+                    false ->
+                        Seed
+                end
+          , guidance =
+                case IsExternal of
+                    true ->
+                        proplists:get_value(guidance, Opts, []);
+                    false ->
+                        []
+                end
+          , variant = proplists:get_value(variant, Opts, undefined)
+          , is_racing_fun = proplists:get_value(is_racing_fun, Opts, fun is_racing/2)
+          , priority_reset_count = 0
+          , max_age = 1000 %% XXX make it configurable
+          }.
 
 enqueue_req(#fd_delay_req{data = Data} = Req, #state{reqs = Reqs, rng = Rng, dequeue_counter = Cnt} =  State) ->
     {P, NewRng} = fd_rand_helper:new_priority(Data, Rng),
@@ -64,7 +65,7 @@ dequeue_req(#state{reqs = Reqs, dequeue_counter = Cnt, guidance = [{Cnt, SeedTer
                   {array:set(Index, {Req, ReqInfo#{last_reset := Cnt, reset := [Cnt - maps:get(last_reset, ReqInfo) | maps:get(reset, ReqInfo)]}, NewP}, CurReqs), NewRng}
           end, {Reqs, rand:seed_s(SeedTerm)}, Reqs),
     dequeue_req(State#state{reqs = NewReqs, rng = NewRng, dequeue_counter = 0, guidance = G});
-dequeue_req(#state{reqs = Reqs, rng = Rng, dequeue_counter = Cnt, priority_reset_count = PRCnt, variant = Variant} = State) ->
+dequeue_req(#state{reqs = Reqs, rng = Rng, dequeue_counter = Cnt, priority_reset_count = PRCnt, variant = Variant, is_racing_fun = IsRacingFun} = State) ->
     {I, _} = array:foldl(
                fun (I, {_, _, P}, none) ->
                        {I, P};
@@ -86,8 +87,10 @@ dequeue_req(#state{reqs = Reqs, rng = Rng, dequeue_counter = Cnt, priority_reset
                                 case Variant of
                                     simrw ->
                                         {TRng, Info, true};
+                                    basic ->
+                                        {TRng, Info, false};
                                     _ ->
-                                        case is_racing(Req, ReqB) of
+                                        case IsRacingFun(Req, ReqB) of
                                             true ->
                                                 case Variant of
                                                     undefined ->
